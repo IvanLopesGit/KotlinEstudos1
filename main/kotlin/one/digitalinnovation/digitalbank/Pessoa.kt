d6000163979d7964abe3b90f2d5a25fd8113be01
Ivan <calintz1@hotmail.com>
2021-02-15 14:47:35 -0300

    Update arquivos aula

--- main/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
+++ main/kotlin/one/digitalinnovation/digitalbank/Pessoa.kt
@@ -1,18 +1,10 @@
 package one.digitalinnovation.digitalbank
 
-class Pessoa {
-    var nome: String = "Ivan"
-    var cpf: String = "123.456.789-00"
-    private set
+abstract class Pessoa(
+        val nome: String,
+        val cpf: String
+) {
 
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
 
 }
 
-fun main() {
-    val ivan = Pessoa()
-
-    println(ivan.pessoaInfo())
-}
